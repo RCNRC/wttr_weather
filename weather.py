c83b3c3d1af774dca69ef9e3f588966663006b68
Ilya <Dragoilya@yandex.ru>
2022-09-26 15:39:52 +0300

    Restructured code and changed README.md

--- weather.py
+++ weather.py
@@ -1,16 +1,26 @@
+"""Print weather of London, svo and Cherepovets"""
+from urllib.error import HTTPError
 import requests
 
-def get_resonse(url, payload):
+
+def print_response(url, payload):
+    """Prints response from http request GET to website url with payload params"""
     response = requests.get(url, params=payload)
-    try:
-        response.raise_for_status()
-        print(response.text)
-    except Exception:
-        print("Response error")
+    response.raise_for_status()
+    print(response.text)
 
 
-url = 'https://wttr.in/'
-places = ['london', 'svo', 'Cherepovets']
-payload = {"m":"", "M":"", "n":"", "T":"", "q":"", "lang":"ru"}
-for place in places:
-    get_resonse(url+place, payload)
+def main() -> None:
+    """Main method"""
+    url = 'https://wttr.in/'
+    places = ['london', 'svo', 'Cherepovets']
+    payload = {'m': '', 'M': '', 'n': '', 'T': '', 'q': '', 'lang': 'ru'}
+    for place in places:
+        try:
+            print_response(f"{url}{place}", payload)
+        except HTTPError:
+            print("HTTPError occure")
+
+
+if __name__ == '__main__':
+    main()
