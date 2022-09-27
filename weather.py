23622083d8d45da0312cd0315664abaef0cacf0e
Ilya <Dragoilya@yandex.ru>
2022-09-27 14:21:45 +0300

    Changed script according to PEP8

--- weather.py
+++ weather.py
@@ -1,25 +1,24 @@
-"""Print weather of London, svo and Cherepovets"""
-from urllib.error import HTTPError
+'''
+Скрипт запрашивает прогноз погоды с сайта https://wttr.in/.
+Скрипт не обрабатывает переданные ему парметры.
+'''
 import requests
 
 
-def print_response(url, payload):
-    """Prints response from http request GET to website url with payload params"""
-    response = requests.get(url, params=payload)
-    response.raise_for_status()
-    print(response.text)
-
-
 def main() -> None:
-    """Main method"""
+    '''
+    По уже установленным адрессам и набору параметров отправляются 3 запрроса GET.
+    Если запрос был удачным, то выводится текст ответа, иначе текст ошибки.
+    '''
     url = 'https://wttr.in/'
     places = ['london', 'svo', 'Cherepovets']
     payload = {'m': '', 'M': '', 'n': '', 'T': '', 'q': '', 'lang': 'ru'}
     for place in places:
-        try:
-            print_response(f"{url}{place}", payload)
-        except HTTPError:
-            print("HTTPError occure")
+        response = requests.get(f"{url}{place}", params=payload)
+        if response.ok:
+            print(response.text)
+        else:
+            print(response.raise_for_status())
 
 
 if __name__ == '__main__':
