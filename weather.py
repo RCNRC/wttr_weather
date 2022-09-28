b1f37e4c853e82879d2c8bc2499e96556a15008f
Ilya <Dragoilya@yandex.ru>
2022-09-28 18:09:07 +0300

    Added requests version and changed raise_for_status position

--- weather.py
+++ weather.py
@@ -1,24 +1,14 @@
-'''
-Скрипт запрашивает прогноз погоды с сайта https://wttr.in/.
-Скрипт не обрабатывает переданные ему парметры.
-'''
 import requests
 
 
 def main() -> None:
-    '''
-    По уже установленным адрессам и набору параметров отправляются 3 запрроса GET.
-    Если запрос был удачным, то выводится текст ответа, иначе текст ошибки.
-    '''
     url = 'https://wttr.in/'
     places = ['london', 'svo', 'Cherepovets']
     payload = {'m': '', 'M': '', 'n': '', 'T': '', 'q': '', 'lang': 'ru'}
     for place in places:
         response = requests.get(f"{url}{place}", params=payload)
-        if response.ok:
-            print(response.text)
-        else:
-            print(response.raise_for_status())
+        response.raise_for_status()
+        print(response.text)
 
 
 if __name__ == '__main__':
